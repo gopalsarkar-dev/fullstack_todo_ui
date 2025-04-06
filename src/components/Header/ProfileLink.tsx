69bb26962acda531c3f6078564b974036fef841f
Gopal Sarkar <gopalsarkar.dev@gmail.com>
2025-04-06 12:36:30 +0530

    getCurrentUserProfile done

--- src/components/Header/ProfileLink.tsx
+++ src/components/Header/ProfileLink.tsx
@@ -1,10 +1,18 @@
 import Link from "next/link";
+import getCurrentUserProfile from "../hooks/profile/getCurrentUserProfile";
 
 const ProfileLink = async () => {
+  const { data, error, isError } = await getCurrentUserProfile();
+
+  if (isError) {
+    console.log(error);
+    return null;
+  }
+
   return (
     <>
       <Link href="/profile" className="underline">
-        Hellow, <span>Gopal</span>
+        Hellow, <span>{data?.first_name}</span>
       </Link>
     </>
   );
