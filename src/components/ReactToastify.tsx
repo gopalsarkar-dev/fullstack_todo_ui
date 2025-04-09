465067df38dc2f101f2073e029e535ffbc5bafdc
Gopal Sarkar <gopalsarkar.dev@gmail.com>
2025-04-09 08:19:36 +0530

    react-toastify theme

--- src/components/ReactToastify.tsx
+++ src/components/ReactToastify.tsx
@@ -1,10 +1,16 @@
 "use client";
+import { useTheme } from "next-themes";
 import { ToastContainer } from "react-toastify";
 
 const ReactToastify = () => {
+  const { theme } = useTheme();
+
   return (
     <>
-      <ToastContainer autoClose={500} />
+      <ToastContainer
+        autoClose={500}
+        theme={theme === "light" ? "light" : "dark"}
+      />
     </>
   );
 };
